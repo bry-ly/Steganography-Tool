dc236fd5b5bd6eab5252603dab421763aca3411d
bry-ly <bryanpalay119@gmail.com>
2026-06-15 20:12:33 +0800

    feat: register Tabs, Steps, Callout, CodeBlock, Files, Card in MDX components

--- components/mdx.tsx
+++ components/mdx.tsx
@@ -1,9 +1,29 @@
 import defaultMdxComponents from "fumadocs-ui/mdx";
 import type { MDXComponents } from "mdx/types";
+import { Tab, Tabs } from "fumadocs-ui/components/tabs";
+import { CodeBlock, Pre } from "fumadocs-ui/components/codeblock";
+import { Step, Steps } from "fumadocs-ui/components/steps";
+import { Callout } from "fumadocs-ui/components/callout";
+import { File, Folder, Files } from "fumadocs-ui/components/files";
+import { Card } from "fumadocs-ui/components/card";
 
 export function getMDXComponents(components?: MDXComponents) {
   return {
     ...defaultMdxComponents,
+    pre: ({ ref: _ref, ...props }) => (
+      <CodeBlock>
+        <Pre {...props}>{props.children}</Pre>
+      </CodeBlock>
+    ),
+    Tabs,
+    Tab,
+    Steps,
+    Step,
+    Callout,
+    Files,
+    Folder,
+    File,
+    Card,
     ...components,
   } satisfies MDXComponents;
 }
