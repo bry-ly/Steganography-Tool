c530331aace7c699c7ba6fe7492558b34fc93aa2
bry-ly <bryanpalay119@gmail.com>
2026-06-15 19:00:06 +0800

    feat: add documentation pages (getting started, guides, API reference)

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono, JetBrains_Mono } from "next/font/google";
 import { ThemeProvider } from "next-themes";
+import { RootProvider } from "fumadocs-ui/provider/next";
 import "./globals.css";
 import { cn } from "@/lib/utils";
 import { Toaster } from "@/components/ui/sonner";
@@ -23,8 +24,10 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
     >
       <body className="min-h-full flex flex-col font-mono">
         <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
-          {children}
-          <Toaster />
+          <RootProvider>
+            {children}
+            <Toaster />
+          </RootProvider>
         </ThemeProvider>
       </body>
     </html>
