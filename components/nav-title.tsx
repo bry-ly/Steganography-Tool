8fcce24270c4a8b60ad2f271b6a69741926c9788
bry-ly <bryanpalay119@gmail.com>
2026-06-26 00:26:48 +0800

    refactor(nav): reuse logo component

--- components/nav-title.tsx
+++ components/nav-title.tsx
@@ -1,21 +1,8 @@
 "use client";
 
-import Image from "next/image";
-import Link from "next/link";
+import { Logo } from "@/components/logo";
 import type { ComponentProps } from "react";
 
 export function NavTitle(props: ComponentProps<"a">) {
-  return (
-    <Link href="/" {...props}>
-      <Image
-        src="/logo/Logo.png"
-        alt="Logo"
-        width={50}
-        height={50}
-        className="rounded"
-        priority
-      />
-      <span>StegnoHide</span>
-    </Link>
-  );
+  return <Logo size={50} {...props} />;
 }
